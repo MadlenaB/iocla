58126d2e5ad7f8e6aecc78bc91d87edb3437ed2b
MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>
2024-03-29 21:32:56 +0200

    Lab4-ex4- implementation of binary search with goto
    Signed-off-by: MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>

--- laborator/content/toolchain-decompilare/3-goto-algs/b-binary_search/binary_search.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/b-binary_search/binary_search.c
@@ -9,8 +9,24 @@ int main(void)
 	int start = 0;
 	int end = sizeof(v) / sizeof(int) - 1;
 
+	int middle;
 	/* TODO: Implement binary search */
-	(void) dest;
-	(void) start;
-	(void) end;
+	// (void) dest;
+	// (void) start;
+	// (void) end;
+
+dest:
+	if(start == end)
+		goto end;
+	middle = (start + end) / 2;
+	if(v[middle] < dest) 
+		goto start;
+	end = middle; 
+	goto dest;
+start:
+	start = middle + 1;
+	goto dest;
+end:
+	printf("Am gasit dest %d la pozitia %d\n", dest, start);
+return 0;
 }
