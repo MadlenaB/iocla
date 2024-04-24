2c82776cd522a8d682c51481b3b7c0c3027a2382
MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>
2024-04-24 19:24:08 +0300

    This is the solution for problem 3 and 4. Print the string, int and char and than copy another values.
    Signed-off-by: MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>

--- laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
+++ laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
@@ -26,23 +26,29 @@ main:
     push ebp
     mov ebp, esp
 
-    ; TODO
-    ; print all three values (int_x, char_y, string_s) from simple_obj
-    ; Hint: use "lea reg, [base + offset]" to save the result of
-    ; "base + offset" into register "reg"
+    PRINTF32 `int_x: %d\n\x0`, [sample_obj + int_x]
+    PRINTF32 `char_y: %c\n\x0`, [sample_obj + char_y]
+    lea eax, [sample_obj + string_s]
+    PRINTF32 `string_s: %s\n\x0`, eax
 
-    ; TODO
-    ; write the equivalent of "simple_obj->int_x = new_int"
+    mov eax, [new_int]
+    mov [sample_obj + int_x], eax
 
-    ; TODO
-    ; write the equivalent of "simple_obj->char_y = new_char"
+    mov al, [new_char]
+    mov [sample_obj + char_y], al
 
-    ; TODO
-    ; write the equivalent of "strcpy(simple_obj->string_s, new_string)"
+    mov ecx, 0
+copy:
+    mov bl, [new_string + ecx]
+    mov [sample_obj + string_s + ecx], bl
+    inc ecx
+    cmp bl, 0
+    jnz copy
 
-    ; TODO
-    ; print all three values again to validate the results of the
-    ; three set operations above
+    PRINTF32 `int_x: %d\n\x0`, [sample_obj + int_x]
+    PRINTF32 `char_y: %c\n\x0`, [sample_obj + char_y]
+    lea eax, [sample_obj + string_s]
+    PRINTF32 `string_s: %s\n\x0`, eax
 
     xor eax, eax
     leave
